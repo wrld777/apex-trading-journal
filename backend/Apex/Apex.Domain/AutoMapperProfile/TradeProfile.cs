755818999c4516d38b559bae5626e797d9e53c7a
ahmed <ahmedbejaoui292@gmail.com>
2026-05-21 22:32:57 +0200

    feat: trade service - AutoMapper, Result pattern, CancellationToken

---
+++ backend/Apex/Apex.Domain/AutoMapperProfile/TradeProfile.cs
@@ -0,0 +1,17 @@
+﻿using Apex.Domain.DTOs;
+using Apex.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apex.Domain.AutoMapperProfile
+{
+    public class TradeProfile : Profile
+    {
+        public TradeProfile()
+        {
+            CreateMap<Trade, TradeDto>().ReverseMap();
+        }
+    }
+}
