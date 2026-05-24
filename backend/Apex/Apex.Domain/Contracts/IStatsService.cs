3e2502457fb0e0de64f7577027990c26cb51096a
ahmed <ahmedbejaoui292@gmail.com>
2026-05-24 22:26:59 +0200

    StatsService implementation

---
+++ backend/Apex/Apex.Domain/Contracts/IStatsService.cs
@@ -0,0 +1,15 @@
+﻿using Apex.Domain.Common;
+using Apex.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apex.Domain.Contracts
+{
+    public interface IStatsService
+    {
+        Task<Result<StatsDto>> GetStatsByUserAsync(Guid userId, CancellationToken ct);
+        Task<Result<StatsDto>> GetStatsByDateRangeAsync(Guid userId, DateTime from, DateTime to, CancellationToken ct);
+
+    }
+}
